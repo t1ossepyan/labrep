7787899e69c835f4b95506824d9ff5ef1b80740d
t1ossepyan <t_ossepyan@kbtu.kz>
2025-03-27 17:52:49 +0500

    lab8

--- lab8/racer.py
+++ lab8/racer.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 import random
-import time
+
 
 W = 300
 H = 800
@@ -10,13 +10,21 @@ H = 800
 car = pygame.image.load("/Users/tevososepyan/Documents/PythonRep/lab8/sprites/unnamed.png")
 car = pygame.transform.scale(car, (100, 100))
 
+score = 0
+
 enemy = pygame.image.load("/Users/tevososepyan/Documents/PythonRep/lab8/sprites/938z8l9w2ho51.png.webp")
 sprite_img = pygame.transform.scale(enemy, (100, 100))
 sprite_size = sprite_img.get_rect().size
 
 screen = pygame.display.set_mode((W, H))
 
+bg = pygame.image.load("/Users/tevososepyan/Documents/PythonRep/lab8/sprites/New Project-2.png")
 
+coin = pygame.image.load("/Users/tevososepyan/Documents/PythonRep/lab8/sprites/2140382.png")
+coin = pygame.transform.scale(coin, (100, 100))
+coin_size = coin.get_rect().size
+
+coins=[]
 sprites = []
 
 cordx = 0
@@ -28,6 +36,8 @@ font = pygame.font.Font(None, 36)
 
 running = True
 
+
+
 while running:
     for i in pygame.event.get():
         if i.type == pygame.QUIT:
@@ -35,43 +45,68 @@ while running:
             sys.exit()
     screen.fill((255, 255, 255))
 
+    screen.blit(bg, (0, 0))
 
-
-    text = font.render(f"x {cordx}", True, (0, 0, 0))
+    text = font.render(f"Монет собрано: {score}", True, (0, 0, 0))
     text_rect = text.get_rect(topleft=(10, 10))
 
     screen.blit(text, text_rect)
 
-
+    
     keys = pygame.key.get_pressed()
+
+    
+
     if keys[pygame.K_RIGHT] and cordx <= 200:
-        cordx += 1
+        if keys[pygame.K_LSHIFT]:
+            cordx += 0.5
+        else:
+            cordx += 1
     if keys[pygame.K_LEFT] and cordx >= 0:
-        cordx -= 1
+        if keys[pygame.K_LSHIFT]:
+            cordx -= 0.5
+        else:
+            cordx -= 1
 
 
     if random.random() < 0.001:
-        x = random.randint(0, 2)  # Случайное место сверху
-        y = -sprite_size[1]  # Старт выше экрана
-        speed = 1  # скорость падения
+        x = random.randint(0, 2) 
+        y = -sprite_size[1] 
+        speed = 1  
         sprites.append([x*100, y, speed])
 
-    # Обновляем позиции спрайтов
     for sprite in sprites:
-        sprite[1] += sprite[2]  # Двигаем вниз
+        sprite[1] += sprite[2] 
 
-    # Удаляем спрайты, если они выходят за нижний край
     sprites = [s for s in sprites if s[1] < H]
 
-    # Рисуем спрайты
     for sprite in sprites:
         screen.blit(sprite_img, (sprite[0], sprite[1]))
 
     for sprite in sprites:
         if sprite[0] <= cordx <= sprite[0]+100 and sprite[1] <= cordy <= sprite[1]+100 or sprite[0] <= cordx+100 <= sprite[0]+100 and sprite[1] <= cordy+100 <= sprite[1]+100:
-            print("Столкновение!")
             running = False
             sys.exit()
+
+    if random.random() < 0.0001:
+        x = random.randint(0, 2) 
+        y = -coin_size[1] 
+        speed = 1 
+        coins.append([x*100, y, speed])
+
+    for i in coins:
+        i[1] += i[2] 
+
+    coins = [s for s in coins if s[1] < H]
+
+    for i in coins:
+        screen.blit(coin, (i[0], i[1]))
+
+    for i in coins:
+        if i[0] <= cordx <= i[0]+100 and i[1] <= cordy <= i[1]+100 or i[0] <= cordx+100 <= i[0]+100 and i[1] <= cordy+100 <= i[1]+100:
+            score += 1
+            coins.pop()
+
 
 
     screen.blit(car, (cordx, cordy))
